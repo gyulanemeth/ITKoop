1479d9a10150dece69de18e3a7a987fe3157acf6
Varga Tamas <tamas@alapzaj.com>
2012-03-09 14:27:11 +0100

    Kis fix, mashova ment a binaris

--- let_me_help_you.sh
+++ let_me_help_you.sh
@@ -22,7 +22,7 @@ function build(){
 	echo "Creating console_build directory"
 	mkdir -p CoopServer/console_build
 	echo "running javac"
-	javac -classpath /tmp/badassdeps/jWebSocket-1.0/libs/jWebSocketServer-1.0.jar:CoopServer/src -d CoopServer/bin CoopServer/src/server/cooproject/itk/hu/*.java
+	javac -classpath /tmp/badassdeps/jWebSocket-1.0/libs/jWebSocketServer-1.0.jar:CoopServer/src -d CoopServer/console_build CoopServer/src/server/cooproject/itk/hu/*.java
 	echo "done"
 
 }
