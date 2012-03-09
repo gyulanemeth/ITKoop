002e22cc4917b6f9644c4d069dabe58651637769
Varga Tamas <tamas@alapzaj.com>
2012-03-09 13:39:53 +0100

    MessageListener hozzaadva

---
+++ CoopServer/src/server/cooproject/itk/hu/coopMessageListener.java
@@ -0,0 +1,78 @@
+package server.cooproject.itk.hu;
+
+import org.jwebsocket.api.WebSocketPacket;
+import org.jwebsocket.factory.JWebSocketFactory;
+import org.jwebsocket.kit.WebSocketServerEvent;
+import org.jwebsocket.listener.WebSocketServerTokenEvent;
+import org.jwebsocket.listener.WebSocketServerTokenListener;
+import org.jwebsocket.server.TokenServer;
+import org.jwebsocket.token.Token;
+
+
+public class coopMessageListener implements WebSocketServerTokenListener{
+	private TokenServer _tokenServer;
+	
+	/**
+	 * Built in
+	 * @return
+	 */
+	public TokenServer getTokenServer(){
+		return _tokenServer;
+	}
+	
+	public void init(){
+		try{
+			/* Start the factory */
+			JWebSocketFactory.start();
+			_tokenServer = (TokenServer) JWebSocketFactory.getServer("ts0");
+			if(_tokenServer != null ){
+				System.out.println("Server is running!");
+				_tokenServer.addListener(this);
+			}else{
+				System.out.println("Server is not running!");
+			}
+			
+		}catch(Exception e){
+			System.out.println("Error:"+e.getMessage());
+			
+		}
+		
+		
+	}
+	
+	@Override
+	public void processClosed(WebSocketServerEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void processOpened(WebSocketServerEvent arg0) {
+		System.out.println("New client connected to our websocket server");
+		
+	}
+
+	@Override
+	public void processPacket(WebSocketServerEvent arg0, WebSocketPacket arg1) {
+		System.out.println("Packet received: "+arg1.getString());
+		
+	}
+
+	@Override
+	public void processToken(WebSocketServerTokenEvent tEvent, Token receivedToken) {
+		System.out.println("New token: "+receivedToken.getNS()+" -> "+receivedToken.getType());
+		/* simple echo */
+		  // create a response token
+	      Token echoResponse = tEvent.createResponse(receivedToken);
+	      echoResponse.setString("code", "-1");
+	      echoResponse.setString("msg", "Ping - Pong");
+	      tEvent.sendToken(echoResponse);
+	      /* Simple broadcast */
+	      Token brToken = tEvent.createResponse(receivedToken);
+	      brToken.setString("code", "255");
+	      brToken.setString("msg", "This is a broadcast message");
+	      _tokenServer.broadcastToken(brToken);
+	}
+	
+	
+}
--- CoopServer/src/server/cooproject/itk/hu/serverMain.java
+++ CoopServer/src/server/cooproject/itk/hu/serverMain.java
@@ -2,4 +2,9 @@ package server.cooproject.itk.hu;
 
 public class serverMain {
 
+	public static void main(String[] args){
+		coopMessageListener _cmListener = new coopMessageListener();
+		_cmListener.init();	
+	}
+	
 }
