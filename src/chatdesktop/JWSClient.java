e901d207d3431395775987e54fc3c0f148fd400c
CBA <Chuckie@ChuckiePC.(none)>
2012-05-08 00:56:36 +0200

    Gomez - delete node

--- src/chatdesktop/JWSClient.java
+++ src/chatdesktop/JWSClient.java
@@ -13,7 +13,7 @@ import org.jwebsocket.kit.WebSocketException;
 import org.jwebsocket.token.Token;
 import org.jwebsocket.token.TokenFactory;
 /**
- *
+ * kliens szerver kapcsolat kezelese, krealja, kuldi az uzeneteket, fogadja, kezeli azokat
  * @author Sallai Gergely
  */
 public class JWSClient implements WebSocketClientTokenListener{
@@ -34,10 +34,28 @@ public class JWSClient implements WebSocketClientTokenListener{
            }
         return _instance;
     }
+    /**
+     * lehet favagas, de oda kell vhogy adni neki a canvast, aminek kuldi az uzeneteket
+     * @param canvas ezen van minden grafikus cumo
+     */
     public void setCanvas(Canvas canvas){
         this.canvas=canvas;
     }
+    /**
+     * Elozohoz hasonloan a desktopot is oda adjuk neki
+     * @param form 
+     */
+    void setDesktop(ChatDesktop form) {
+        this.form=form;
+    }
     
+    /**
+     * Kivulrol hivhato, connectet es autentikaciot kezdemenyez
+     * @param name bejelentkezési név, el is tároljuk
+     * @param pass jelszo...
+     * @param secure secure WebSocketet hasznaljunk vagy sem
+     * @return true ha minden ok false ha nem
+     */
     public boolean login(String name, String pass, boolean secure) {
         this.userName = name;
         try {
@@ -55,6 +73,11 @@ public class JWSClient implements WebSocketClientTokenListener{
             return false;
         }
     }
+    
+    /**
+     * Disconnectel a szerverrol, lezarja a kapcsolatot, semmi tobb.
+     * @return true ha nincs hiba, false ha van
+     */
     public boolean disconnect(){
         try {
             tClient.disconnect();
@@ -66,6 +89,12 @@ public class JWSClient implements WebSocketClientTokenListener{
         return true;
     }
     
+    /**
+     * Ez a belso method rendezi a konkret autentikaciot
+     * 0 type-u message a login request
+     * @param pass jelszooo
+     * @return true ha nincs hiba, false ha van
+     */
     private boolean sendLogin(String pass)
     {
         try {
@@ -80,6 +109,12 @@ public class JWSClient implements WebSocketClientTokenListener{
         }
     }
     
+    /**
+     * Egyszeru chat uzenet kuldese, type 1000-el.
+     * @param userName sajat nevunk, amivel bejelentkeztunk
+     * @param text a szoveg amit kuldeni szeretnenk.
+     * @return true ha minden ok, false ha para van.
+     */
     public boolean sendText(String userName, String text) {
         try {
             Token token = getMessageBone(1000);
@@ -95,9 +130,20 @@ public class JWSClient implements WebSocketClientTokenListener{
         }
     }
     
+    /**
+     * Mozgatasi esemeny kuldese a szervernek (type 2 vagy 4)
+     * @param userName kuldo neve (mi nevunk)
+     * @param objId az objektum azonositoja
+     * @param x az objektum uj X koordinataja
+     * @param y az objektum uj Y koordinataja
+     * @param savepos uzenet a szervernek h eltarolja a DB-ben a valtozast
+     *  vagy meg ne, mert nem fejeztuk be a mozgatast
+     * @return ha minden rendben akkor true, ha hiba van akkor false
+     */
     public boolean sendMoveObject(String userName, String objId, Integer x, Integer y, Boolean savepos){
         try {
-            Map<String, String> message = new HashMap<>();
+            //Map<String, String> message = new HashMap<>();
+            Map message = new HashMap();
             message.put("userName", userName);
             message.put("objId", objId);
             message.put("x", x.toString());
@@ -110,7 +156,8 @@ public class JWSClient implements WebSocketClientTokenListener{
             else
                 token = getMessageBone(4);
             token.setMap("message", message);
-            //System.out.println("OUT Move: "+token.toString());
+            
+            System.out.println("OUT Move: "+token.toString());
             tClient.sendToken(token);
             return true;
         } catch (WebSocketException ex) {
@@ -119,6 +166,14 @@ public class JWSClient implements WebSocketClientTokenListener{
         }
     }
     
+    /**
+     * Uj objektum letrehozasanak kerese a szervertol (type 3)
+     * @param exampleString Az uj object szovege
+     * @param x koordinata
+     * @param y koordinata
+     * @param z koordinata
+     * @return true ha ok, false ha hiba
+     */
     public boolean sendCreateObject(String exampleString, Integer x,Integer y,Integer z){
         try {
             Token token = getMessageBone(3);
@@ -138,14 +193,17 @@ public class JWSClient implements WebSocketClientTokenListener{
         }
     }
     
+    /**
+     * Objektum torles kerese. type 5
+     * @param objectId Objektum azonositoja
+     * @return true ha minden ok, false ha hiba van
+     */
     public boolean sendDeleteObject(String objectId){
-        throw new UnsupportedOperationException("Not supported yet.");
-        /*try {
-            // TODO milyen TYPE?
-            Map<String, String> message = new HashMap<>();
-            message.put("objId", objectId);
+        try {
+            Token token = getMessageBone(5);
             
-            Token token = getMessageBone(?)
+            Map message = new HashMap();
+            message.put("objId", objectId);
             token.setMap("message", message);
             
             System.out.println("OUT Delete: "+token.toString());
@@ -154,9 +212,15 @@ public class JWSClient implements WebSocketClientTokenListener{
         } catch (WebSocketException ex) {
             Logger.getLogger(JWSClient.class.getName()).log(Level.SEVERE, null, ex);
             return false;
-        }*/
+        }
     }
     
+    /**
+     * Objektum modositas kuldese. (type 1)
+     * @param objId
+     * @param exampleString
+     * @return true ha ok, false ha hiba
+     */
     public boolean sendModifyObject(String objId, String exampleString){
         try {
             Token token = getMessageBone(1);
@@ -175,6 +239,12 @@ public class JWSClient implements WebSocketClientTokenListener{
         }
     }
     
+    /**
+     * Bejovo token/csomag szetvalogatasa.
+     * A megfelelo helyre kuldi feldolgozasra.
+     * @param wsce WebSocket event
+     * @param token maga a bejovo csomag
+     */
     @Override
     public void processToken(WebSocketClientEvent wsce, Token token) {
         System.out.println("IN: "+token.toString());
@@ -183,12 +253,13 @@ public class JWSClient implements WebSocketClientTokenListener{
         if(token.getString("type") != null &&
             token.getString("type").equals("event")) return; //csak a vmi nemtom milyen event jott
 
+        // Dolgozzuk fel a letezo fieldeket
         try{
-            // Dolgozzuk fel a letezo fieldeket
+            // uzenet tipusat kiszedjuk
             int cType = 0;
+            // favagas.. biztosa megyunk, vagy stringkent vagy intkent benne lesz
             if (token.getString("type") != null) {
                 cType = Integer.parseInt(token.getString("type"));
-                //cType = token.getInteger("type");
             }
             if (token.getInteger("type") != null) {
                 cType = token.getInteger("type");
@@ -211,17 +282,22 @@ public class JWSClient implements WebSocketClientTokenListener{
                     System.out.println("It's a move");
                     handleMove(token);
                     break;
-                // Create Object!
+                // Create Object.
                 case 3:
                     System.out.println("It's a create");
                     handleCreate(token);
                     break;
-                // MoveObject mentes nelkul!
+                // MoveObject mentes nelkul.
                 case 4:
                     System.out.println("It's a move");
                     handleMove(token);
                     break;
-                // Egy chat message. Egyelore csak broadcastoljuk
+                // Delete Object
+                case 5:
+                    System.out.println("It's a delete");
+                    handleDelete(token);
+                    break;
+                // Egy chat message.
                 case 1000:
                     System.out.println("It's a chat");
                     handleChatMessage(cSenderName, cMessage);
@@ -232,47 +308,79 @@ public class JWSClient implements WebSocketClientTokenListener{
         }
     }
 
+    /**
+     * Default event, nem hasznalom...
+     * @param wsce 
+     */
     @Override
     public void processOpening(WebSocketClientEvent wsce) {
         //throw new UnsupportedOperationException("Not supported yet.");
         System.out.println("processOpening");
     }
-
+    
+    /**
+     * Default event, nem hasznalom...
+     * @param wsce 
+     */
     @Override
     public void processOpened(WebSocketClientEvent wsce) {
         //throw new UnsupportedOperationException("Not supported yet.");
         System.out.println("processOpened");
     }
-
+    
+    /**
+     * Default event, nem hasznalom...
+     * @param wsce
+     * @param wsp 
+     */
     @Override
-    public void processPacket(WebSocketClientEvent wsce, WebSocketPacket wsp) {
-        
+    public void processPacket(WebSocketClientEvent wsce, WebSocketPacket wsp) {      
         //throw new UnsupportedOperationException("Not supported yet.");
     }
-
+    
+    /**
+     * A kapcsolat lezarulasakor hivodik.
+     * Nem igazan hasznalom, csak jelzem h volt ilyen is.
+     * @param wsce 
+     */
     @Override
     public void processClosed(WebSocketClientEvent wsce) {
-        //throw new UnsupportedOperationException("Not supported yet.");
         System.out.println("Kiléptünk!");
     }
-
+    
+    /**
+     * Default event, nem hasznalom...
+     * @param wsce 
+     */
     @Override
     public void processReconnecting(WebSocketClientEvent wsce) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
-
-    void setDesktop(ChatDesktop form) {
-        this.form=form;
-    }
-    
     
     //-----------------------------------------------------------------
-    /// HANDLES
+    /// HANDLERS
+    /// Uzenet kezelo metodusok, belso hasznalatra.
+    /// Eredetileg kulon osztalyban volt, de ide kerult a sok kavarodas
+    /// es fejetlenseg miatt, es itt maradt.. :(
     
+    /**
+     * Chat uzenetek kezelese.
+     * Szolunk az ablaknak h van uj uzenet.
+     * @param sender
+     * @param message 
+     */
     private void handleChatMessage(String sender, String message) {
         form.newMessage(sender, message);
     }
     
+    /**
+     * Bejovo mozgatasi esemeny kezelese.
+     * Kiszedjuk a token tartalmat, megnezzuk hogy van-e mar ilyen azonositoju
+     * objektum nalunk, ha van akkor a megadott helyre mozgatjuk,
+     * ha nincs akkor a szerver szolt h kene egy uj objektum itt.
+     * Ezert letrehozzuk.
+     * @param token esemenyt tartalmazo csomag..
+     */
     private void handleMove(Token token) {
         Map message=token.getMap("message");
         final String objId = message.get("objId").toString();
@@ -293,6 +401,12 @@ public class JWSClient implements WebSocketClientTokenListener{
         }
     }
     
+    /**
+     * Bejovo modositas kerelem kezelese.
+     * Ha van ilyen objektumunk, akkor modositjuk a tartalmat,
+     * ha nincs akkor eldobjuk az uzenetet.
+     * @param token esemenyt tartalmazo csomag..
+     */
     private void handleModify(Token token) {
         Map message = token.getMap("message");
         String objId = message.get("objId").toString();
@@ -304,6 +418,25 @@ public class JWSClient implements WebSocketClientTokenListener{
         }
     }
     
+    /**
+     * Bejovo torles kerelem kezelese.
+     * Ha van ilyen objektum, akkor toroljuk,
+     * ha nincs akkor eldobjuk a csomagot.
+     * @param token esemenyt tartalmazo csomag
+     */
+    private void handleDelete(Token token) {
+        Map message = token.getMap("message");
+        String objId = message.get("objId").toString();
+        if(canvas.containsObject(objId)){
+            canvas.deleteObject(objId);
+        }
+    }
+    
+    /**
+     * Bejovo objektum letrehozasi kerelem.
+     * Ez a mostani verzioban nem hasznalt, ilyen uzenetet nem kuld a szerver.
+     * @param token 
+     */
     private void handleCreate(Token token) {
         Map message = token.getMap("message");
         final String objId = message.get("objId").toString();
@@ -326,24 +459,36 @@ public class JWSClient implements WebSocketClientTokenListener{
         }
     }
     
+    /**
+     * Login uzenet feldolgozasa.
+     * Ebben a verzioban mar nem jon felenk ilyen uzenet,
+     * eldobjuk a csomagot.
+     * @param aToken 
+     */
     private void handleLogin(Token aToken) {
 		String username = aToken.getString("sender");
-                // TODO
     }
     
+    /**
+     * Uzenet vaz gyarto metodus.
+     * Adott type-hoz kreal egy uzenet vazat, amibe lehet pakolni majd
+     * @param type Ilyen tipusu lesz az uzenet.
+     * @return A kesz uzenet vaza.
+     */
     private Token getMessageBone(int type) {
 	// A recept
         // Fogj 1 message-t
         Token simpleMessage = TokenFactory.createToken();
         // Rakj bele typeot
-        //simpleMessage.setInteger("type", type);
+        simpleMessage.setInteger("type", type);
         simpleMessage.setString("type", Integer.toString(type));
         // Sendert
         simpleMessage.setString("sender", this.userName);
         // Timestampet
         double timestamp = System.currentTimeMillis() / 1000;
-        //simpleMessage.setDouble("timestamp", timestamp);
+        simpleMessage.setDouble("timestamp", timestamp);
         simpleMessage.setString("timestamp", Double.toString(timestamp));
         return simpleMessage;
     }
+
 }
